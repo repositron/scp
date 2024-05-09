b9ef43313ddd39b631f247f7de547ea79c328de9
ljw <liamv7@gmail.com>
2024-05-09 10:12:57 +0900

    client tests

--- src/test/scala/prices/routes/protocol/SmartCloudClientTest.scala
+++ src/test/scala/prices/routes/protocol/SmartCloudClientTest.scala
@@ -10,29 +10,19 @@ import prices.services.smartcloud.{ServerError, SmartCloudClient}
 class SmartCloudClientTest extends CatsEffectSuite:
   // these tests use the smart cloud docker endpoint, and because that can return 500 randomly, these test can fail.
 
-  /*test("when valid kind on docker smart cloud then should return value (requires docker running)") {
-    EmberClientBuilder.default[IO].build.use { ember =>
-      val config = RootConfig(AppConfig("http://localhost/", 8082), SmartcloudConfig("http://localhost:9999/", ""))
-      val sc = SmartCloudClient.make[IO](config, ember)
-
-      sc.getKindPrices("sc2-mirco").flatmap { response =>
-        assertEquals(response.map(.kind, "sc2-mirco2"))
-      }
-    }.unsafe
-  }*/
-
-  test("when request know value it should return value and amount (requested from docker)") {
+  test("when request know value it should return value and amount (requires docker running)") {
     EmberClientBuilder.default[IO].build.use { ember =>
       val config = RootConfig(AppConfig("http://localhost/", 8082), SmartcloudConfig("http://localhost:9999/", "lxwmuKofnxMxz6O2QE1Ogh"))
       val sc = SmartCloudClient.make[IO](config, ember)
 
       sc.getKindPrices("sc2-std-2").map { response =>
-        assertEquals(response, Right(SmartCloudKindInfoResponse("sc2-std-2", 0.4444)))
+        val smartCloudKindInfoResponse = response.getOrElse(fail("failed"))
+        assertEquals(smartCloudKindInfoResponse.kind, "sc2-std-2")
       }
     }.unsafeRunSync()
   }
 
-  test("when unknown kind is request respond with 404") {
+  test("when unknown kind is request respond with 404 (requires docker running)") {
     EmberClientBuilder.default[IO].build.use { ember =>
       val config = RootConfig(AppConfig("http://localhost/", 8082), SmartcloudConfig("http://localhost:9999/", "lxwmuKofnxMxz6O2QE1Ogh"))
       val sc = SmartCloudClient.make[IO](config, ember)
